2303f23fdd82c9405be0be8e69243aaa6991c5ff
Zyryanovvm <102282267+Zyryanovvm@users.noreply.github.com>
2025-08-13 19:36:49 +0300

    Added gitignore (#2)

--- app/crud/common.py
+++ app/crud/common.py
@@ -1,11 +1,12 @@
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.db import AsyncSessionLocal, Base
+from app.core.db import Base
 from app.schemas.common import Common
 
 
 async def create_common_obj(
-    table: Base, instance: Common, session: AsyncSessionLocal
+    table: Base, instance: Common, session: AsyncSession
 ) -> Base:
     """
     Создание объекта в базовой таблице БД
@@ -25,10 +26,19 @@ def get_exist_criteria_by_name(table: Base, name: str):
     return select(table).where(table.name == name).exists()
 
 
-async def check_exist(exist_criteria, session: AsyncSessionLocal) -> int:
+async def check_exist(exist_criteria, session: AsyncSession) -> bool:
     """
     Проверка существования объекта в
     БД
     """
     check_result = await session.execute(select(True).where(exist_criteria))
     return check_result.scalars().first()
+
+
+async def get_all_objects(table: Base, session: AsyncSession):
+    """
+    Получение всех объекто в БД
+    из базовой таблицы
+    """
+    result = await session.execute(select(table))
+    return result.scalars().all()
